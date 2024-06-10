cd8c3e563a206e45f467b85a041ee8ed1a3b87f1
cbjh-4 <jinhyo44@naver.com>
2024-06-10 16:28:42 +0900

    아무것도 없는 상태 깨끗

---
+++ src/test/java/org/zerock/project_dame/ProjectDameApplicationTests.java
@@ -0,0 +1,13 @@
+package org.zerock.project_dame;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjectDameApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
