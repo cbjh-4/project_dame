54b1d970a28b13ddf7b2e44f7896c214d8f75197
cbjh-4 <jinhyo44@naver.com>
2024-06-11 14:28:49 +0900

    *정진효 -테스트 코드 추가1

--- src/main/java/org/zerock/project_dame/TestGitHub.java
+++ src/main/java/org/zerock/project_dame/TestGitHub.java
@@ -1,4 +1,10 @@
 package org.zerock.project_dame;
 
 public class TestGitHub {
+    private int x = 5;
+
+    public static void main() {
+        TestGitHub t = new TestGitHub();
+        System.out.println(t.x);
+    }
 }
